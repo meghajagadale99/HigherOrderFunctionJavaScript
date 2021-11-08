e285c855506c4fea593b37fbb4c9091b0c0c4068
MeghaJagadale <megha.jagadale@thoughtworks.com>
2021-11-08 23:13:00 +0530

    Add.Logic to check the same array if array is provided with filter as true

--- src/filter.spec.js
+++ src/filter.spec.js
@@ -5,4 +5,8 @@ describe('filter', () => {
     it('should return empty array if provided array is empty', () => {
         expect(filter([], "true")).toEqual([]);
     });
+
+    it('should return same array if array is provided with filter as true', () => {
+        expect(filter([1,2,3], "true")).toEqual([1,2,3]);
+    });
 })
