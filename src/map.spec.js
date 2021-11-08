a3452eb670bfa0b93958b44d69ee69c80b1aea32
MeghaJagadale <megha.jagadale@thoughtworks.com>
2021-11-08 22:51:42 +0530

    Add.Logic to check cube array if given cube as param in map along with array

--- src/map.spec.js
+++ src/map.spec.js
@@ -4,5 +4,13 @@ describe('map', () => {
     it('should return empty array if given empty array to map in cube', () => {
         expect(map([], 'cube')).toEqual([]);
     });
+    
+    it('should return same array if given identity as param in map along with array', () => {
+        expect(map([1, 2, 3], 'identity')).toEqual([1, 2, 3]);
+    });
+
+    it('should return cube array if given cube as param in map along with array', () => {
+        expect(map([1, 2, 3], 'cube')).toEqual([1, 8, 27]);
+    });
 
 })
