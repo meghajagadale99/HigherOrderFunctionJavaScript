16e7a548af230a2319e8fce0803359312e8173ed
MeghaJagadale <megha.jagadale@thoughtworks.com>
2021-11-08 23:38:18 +0530

    Add. logic to check if given array is 5,5 then return sum of numbers

--- src/reduce.spec.js
+++ src/reduce.spec.js
@@ -2,7 +2,12 @@ const reduce = require('./reduce');
 
 describe('reduce', () => {
 
-    it ('should return undefine if given array is empty, (x,y) => x+y',()=>{
-        expect(reduce([], (x,y) => x+y)).toEqual(undefined);
+    it('should return undefine if given array is empty, (x,y) => x+y', () => {
+        expect(reduce([], (x, y) => x + y)).toEqual(undefined);
     })
+
+    it('should return 10 if given array is [5,5 ], (x,y) => x+y', () => {
+        expect(reduce([5, 5], (x, y) => x + y)).toEqual(10);
+    })
+
 })
